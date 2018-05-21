1875a1aaf07ec3d34e2a84cc3cf588b5a41d2e3a
uditgulati <uditgulati0@gmail.com>
2018-05-21 14:12:24 +0530

    Add CF ratings scraper

--- scrapers/cf_scraper.py
+++ scrapers/cf_scraper.py
@@ -1,4 +1,35 @@
 import requests
+import datetime
 from bs4 import BeautifulSoup
-import config
 
+
+
+def get_rating(user):
+	page = requests.get("http://www.codeforces.com/profile/"+user)
+	soup = BeautifulSoup(page.content, 'html.parser')
+
+	rating = 0
+	details = []
+
+	info = soup.find(class_="info").find('ul').find('li').find('span')
+
+	rating = int(info.get_text())
+
+	return rating
+
+
+def fetch_ratings():
+	ranks = []
+	users = [line.rstrip('\n') for line in open('scrapers/users3.txt')]
+	for user in users:
+		curr = get_rating(user)
+		ranks.append((curr, user))
+		#print(dict)
+
+	ranks.sort()
+	return ranks
+
+
+def fetch_data():
+	#fetch_submissions()
+	fetch_ranks()
